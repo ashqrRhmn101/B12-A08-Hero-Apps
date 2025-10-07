142d965c0a410ac428f9b9e8a736a755f034a184
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-10-07 20:46:11 +0600

    appsData.json Data & Footer && Layout Added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import NavBar from '../Components/NavBar';
-import { Outlet } from 'react-router';
-import Footer from '../Components/Footer';
+import React from "react";
+import NavBar from "../Components/NavBar";
+import { Outlet } from "react-router";
+import Footer from "../Components/Footer";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <NavBar/>
-            <div>
-                <Outlet/>
-            </div>
-            <Footer/>
-        </div>
-    );
+  return (
+    <div className="flex flex-col min-h-screen ">
+      <NavBar />
+      <div className="flex-1 max-w-screen-xl mx-auto w-full px-4 md:px-8 lg-px-12 py-4 md:py-8 lg-py-12">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
